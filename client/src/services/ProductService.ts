09473fb2776d8d1f10fb90e847ed3b3a870249f4
Sergio Núñez <sergioop722@gmail.com>
2025-03-18 20:58:05 -0500

    validando datos con valibot antes de enviarlos a las REST API

--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,7 +1,23 @@
+import { safeParse } from "valibot"
+import { DraftProductSchema } from "../types"
+
 type ProductData = {
     [k: string]: FormDataEntryValue
 }
 
+// Función para agregar el producto
 export async function addProduct(data: ProductData) {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        })
+        if (result.success) {
+
+        } else {
+            throw new Error('Datos no vpalidos')
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
